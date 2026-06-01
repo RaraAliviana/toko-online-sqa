59794af4b59b7af85400b70b2230a2f501ece717
RaraAliviana <rgumaranti@gmail.com>
2026-06-01 21:10:26 +0700

    Simulasi Broken Build

--- tests/CatalogTest.php
+++ tests/CatalogTest.php
@@ -33,7 +33,7 @@ class CatalogTest extends TestCase
     {
         $result = $this->katalog->searchProduct("Kemeja");
 
-        $this->assertCount(1, $result);
+        $this->assertCount(99, $result);
     }
 
     protected function tearDown(): void
